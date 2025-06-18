d0b8a8d3b076e37cc161d197d0e44d81bff62f85
Agustin0007 <agustinpatat5@gmail.com>
2025-06-18 16:56:52 -0300

    Actualizacion

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_API_URL || 'https://reservas-backend-tau.vercel.app/api';
+console.log('API_URL:', API_URL);
 
 // Canchas
 export const getCourts = () => axios.get(`${API_URL}/courts`);
